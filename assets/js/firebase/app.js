2bf69f26879a70db736f8ef61d68435649fc541c
Giovani Bueno <gbuennno@gmail.com>
2022-08-26 16:29:18 -0300

    add banco de dados e colocado Css

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCt0NbigNrFwya4omxFWFEC8OmzkW1UyFY",
+    authDomain: "projeto-mundo-invertido-73f02.firebaseapp.com",
+    projectId: "projeto-mundo-invertido-73f02",
+    storageBucket: "projeto-mundo-invertido-73f02.appspot.com",
+    messagingSenderId: "387367815239",
+    appId: "1:387367815239:web:7b1f02fd61ec73a62bc962"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+export default app
